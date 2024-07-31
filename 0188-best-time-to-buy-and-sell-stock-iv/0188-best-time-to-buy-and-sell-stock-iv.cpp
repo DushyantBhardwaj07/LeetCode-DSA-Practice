8e6874f9bb50f663d397267a3fd730c4a50c857a
Dushyant Rajeev Bhardwaj <100109472+DushyantBhardwaj07@users.noreply.github.com>
2024-07-31 15:30:33 +0530

    Time: 16 ms (32.24%), Space: 16.8 MB (23.52%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int helper(int i, int buy, vector<int> &prices, int cap, vector<vector<vector<int>>> &dp){
+        if(i >= prices.size() || cap == 0) return 0;
+        if(dp[i][buy][cap] != -1){
+            return dp[i][buy][cap];
+        }
+        int profit = 0;
+        if(buy){
+            profit = max(-prices[i] + helper(i+1, 0, prices, cap, dp), helper(i+1, 1, prices, cap, dp));
+        }
+        else{
+            profit= max(prices[i] + helper(i+1, 1, prices, cap-1, dp), helper(i+1, 0, prices, cap, dp));
+        }
+        return dp[i][buy][cap] = profit;
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        return helper(0, 1, prices, k, dp);
+    }
+};
